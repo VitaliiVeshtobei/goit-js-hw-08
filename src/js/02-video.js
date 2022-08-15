cedf8a2c6595a5c4e022960299e14c46242f0944
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-08-15 15:48:46 +0300

    done task 2 hmw 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,13 @@
 import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframeRef = document.querySelector('#vimeo-player');
 const player = new Vimeo(iframeRef);
+const savedCurrentSeconds = localStorage.getItem('videoplayer-current-time');
 
 const onTimeupdate = function (data) {
-  console.log(data);
+  localStorage.setItem('videoplayer-current-time', data.seconds);
 };
 
-player.on('timeupdate', onTimeupdate);
+player.on('timeupdate', throttle(onTimeupdate, 10000));
+player.setCurrentTime(savedCurrentSeconds);
