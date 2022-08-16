6c8fe5011004b006600ae575cbcd9c9e9d4e6292
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-08-16 15:36:38 +0300

    done hmw8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,5 @@
+import throttle from 'lodash.throttle';
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('input'),
@@ -5,12 +7,29 @@ const refs = {
 };
 
 refs.form.addEventListener('submit', onSubmit);
-refs.input.addEventListener('input', onInputCheange);
+refs.form.addEventListener('input', throttle(onInputCheange, 500));
+const formData = {};
+
+populateInput();
 
 function onSubmit(evt) {
   evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+  console.log(formData);
 }
 
 function onInputCheange(evt) {
-  console.log(evt.target.value);
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function populateInput() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+
+  if (savedMessage) {
+    const savedMessagePars = JSON.parse(savedMessage);
+    refs.input.value = savedMessagePars['email'];
+    refs.textarea.value = savedMessagePars['message'];
+  }
 }
