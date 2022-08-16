fd1509a88705c247069838a6f3dc53ddd1f72b8b
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-08-16 07:22:48 +0000

    Deploying to gh-pages from @ VitaliiVeshtobei/goit-js-hw-08@0ac9e6aa103ec80ed09251429e5df432b70ac990 🚀

---
+++ 03-feedback.5f9326d6.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".feedback-form"),input:document.querySelector("input"),textarea:document.querySelector("textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault()})),e.input.addEventListener("input",(function(e){console.log(e.target.value)}))}();
+//# sourceMappingURL=03-feedback.5f9326d6.js.map
---
+++ 03-feedback.e8f8e009.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),input:document.querySelector("input"),textarea:document.querySelector("textarea")};e.form.addEventListener("submit",(function(e){e.preventDefault()})),e.input.addEventListener("input",(function(e){console.log(e.target.value)}));
+//# sourceMappingURL=03-feedback.e8f8e009.js.map
