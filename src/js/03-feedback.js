0ac9e6aa103ec80ed09251429e5df432b70ac990
VitaliiVeshtobei <99075525+VitaliiVeshtobei@users.noreply.github.com>
2022-08-16 10:22:18 +0300

    start task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+};
 
+refs.form.addEventListener('submit', onSubmit);
+refs.input.addEventListener('input', onInputCheange);
+
+function onSubmit(evt) {
+  evt.preventDefault();
+}
+
+function onInputCheange(evt) {
+  console.log(evt.target.value);
+}
